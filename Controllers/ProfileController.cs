02260f630b7d02d3613bf44ca7f31e78a61de3c4
ziprateekzi <ziprateekzi@gmail.com>
2025-08-21 02:00:38 +0530

    3 page by prateek

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_Project.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            // This action method will return the default view for the Profile page.
+            return View();
+        }
+    }
+}
